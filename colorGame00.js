f12f53dc3008ae6ad35e6fd41134b0f164e3596d
CamilaNieto-Centennial <fourcamilitanieto@gmail.com>
2022-12-12 15:04:21 -0500

    Add Squares As Child Components

--- colorGame00.js
+++ colorGame00.js
@@ -1,14 +1,26 @@
+const Square = () => {
+  return <button></button>
+};
+
 const Board = () => {
   const [player, setPlayer] = React.useState(1);
   let status = `Player ${player}`;
+  function renderSquare(i) {
+    return <Square></Square>
+  }
   return (
     <div
       className="game-board"
-      onClick={(e) => {
+      /*onClick={(e) => {
         setPlayer((player + 1) % 2)
         status = `Player ${player}`;
-      }}
+      }}*/
     >
+      <div className="grid-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
       <div id="info">
         <h1>{status}</h1>
       </div>
